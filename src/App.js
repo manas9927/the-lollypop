a36e6681528258a34dcfcc4060f3f7ab62a6eb8f
Manas Vivek <manasvjbp27@gmail.com>
2023-04-20 15:54:15 +0530

    :sparkles: First version

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { ReactFlashlight } from "react-flashlight";
-import { unsecureClientSideTokenGate } from "tokengate";
 import { ethers } from "ethers";
-// import { GoogleSpreadsheet } from "google-spreadsheet";
 import jointUnrolled from "./assets/images/joint_unrolled.png";
 import jointRolling from "./assets/images/joint_rolling.gif";
 import jointLit from "./assets/images/joint_lit.gif";
-import Countdown from "react-countdown";
+import axios from "axios";
 
 function App() {
   const [buttonPosition, setButtonPosition] = useState({});
   const [isButtonTextVisible, setIsButtonTextVisible] = useState(false);
+  const [walletAddress, setWalletAddress] = useState();
   const [isMintButtonVisible, setIsMintButtonVisible] = useState(false);
-  const [walletAddress, setWalletAddress] = useState("");
   const [isAllowed, setIsAllowed] = useState(false);
   const [isFlashlightEnabled, setIsFlashlightEnabled] = useState(true);
+  const [isConnectWalletButtonVisible, setIsConnectWalletButtonVisible] =
+    useState(true);
   const [isUnrolledJointVisible, setIsUnrolledJointVisible] = useState(false);
   const [isJointRolling, setIsJointRolling] = useState(false);
   const [isJointLit, setIsJointLit] = useState(false);
 
   const backgroundColor = "#000000";
-
-  const contractAddress = "0x2a9f9ea2fa577079dccc3069e188c129cea6bb86";
 
   useEffect(() => {
     setButtonPosition({
@@ -30,36 +28,53 @@ function App() {
     });
   }, []);
 
+  const checkForToken = async (contract, contractAddress, userAddress) => {
+    const name = await contract.name();
+    const symbol = await contract.symbol();
+    const balance = await contract.balanceOf(userAddress, 1);
+
+    console.log(`\nContract address: ${contractAddress}\n`);
+    console.log(`Contract name: ${name}`);
+    console.log(`Contract symbol: ${symbol}`);
+    console.log(`Tokens: ${balance}`);
+
+    await setIsAllowed(balance > 0 ? true : false);
+    await setIsFlashlightEnabled(balance > 0 ? true : false);
+    await setIsMintButtonVisible(balance > 0 ? false : true);
+    await setIsConnectWalletButtonVisible(false);
+  };
+
   const handleConnectWallet = async () => {
     try {
-      const accounts = await window.ethereum.request({
-        method: "eth_requestAccounts",
-      });
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      await provider.send("eth_requestAccounts", []);
-      const signer = provider.getSigner();
+      const metaMaskprovider = new ethers.providers.Web3Provider(
+        window.ethereum
+      );
+      await metaMaskprovider.send("eth_requestAccounts", []);
+      const signer = metaMaskprovider.getSigner();
 
       const userAddress = await signer.getAddress();
-      const _isAllowed = await unsecureClientSideTokenGate({
-        balanceOfThreshold: 10,
+
+      await setWalletAddress(userAddress);
+
+      const provider = new ethers.providers.JsonRpcProvider(
+        "https://eth-mainnet.g.alchemy.com/v2/2q4OzDyNjd6xXrRI7OgpURgA-XMRwEK1"
+      );
+
+      const ERC20_ABI = [
+        "function name() view returns (string)",
+        "function symbol() view returns (string)",
+        "function totalSupply() view returns (uint256)",
+        "function balanceOf(address, uint) view returns (uint)",
+      ];
+
+      const contractAddress = "0x2a9f9ea2fa577079dccc3069e188c129cea6bb86"; // DAI Contract
+      const contract = new ethers.Contract(
         contractAddress,
-        signerOrProvider: signer,
-        tokenId: 1, // only one token ID exist
-        tokenStandard: "erc1155",
-        userAddress,
-      });
+        ERC20_ABI,
+        provider
+      );
 
-      await setIsAllowed(_isAllowed);
-
-      setIsFlashlightEnabled(false);
-
-      console.log({ walletAddress: accounts[0], isAllowed });
-
-      if (!isAllowed) {
-        setIsMintButtonVisible(true);
-      }
-
-      setWalletAddress(accounts[0]);
+      checkForToken(contract, contractAddress, userAddress);
     } catch {
       console.log("Error connecting");
     }
@@ -79,8 +94,12 @@ function App() {
     setIsButtonTextVisible(false);
   };
 
-  const handleOpenCollectionUrl = () => {
+  const handleOpenLollypop = () => {
     window.open("https://app.manifold.xyz/c/thelollypop");
+  };
+
+  const handleOpenLollypuff = () => {
+    window.open("https://app.manifold.xyz/c/lollypuff");
   };
 
   useEffect(() => {
@@ -94,9 +113,26 @@ function App() {
     setIsJointRolling(true);
   };
 
-  const handleLightJoint = () => {
-    setIsJointRolling(false);
-    setIsJointLit(true);
+  const handleLightJoint = async () => {
+    await setIsUnrolledJointVisible(false);
+    await setIsJointRolling(false);
+    await setIsJointLit(true);
+    addToAllowList();
+  };
+
+  const addToAllowList = () => {
+    axios
+      .post(
+        "https://api.retool.com/v1/workflows/4f96456a-3fb3-41a6-8b74-85ce4accd956/startTrigger?workflowApiKey=retool_wk_b7e421520dd54aa0b25f396a39e4e00f",
+        `${walletAddress}`,
+        { headers: { "Content-Type": "application/json" } }
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log({ err });
+      });
   };
 
   return (
@@ -111,7 +147,7 @@ function App() {
           isFlashlightEnabled ? `bg-[${backgroundColor}]` : "bg-white"
         }`}
       >
-        {isFlashlightEnabled && (
+        {isFlashlightEnabled && isConnectWalletButtonVisible && (
           <button
             className={`hover:border-white hover:border-4 bg-[${backgroundColor}] font-bold px-10 py-4 w-56 rounded-lg hover:text-white transform hover:scale-110 absolute`}
             style={{ top: buttonPosition.y, right: buttonPosition.x }}
@@ -124,43 +160,19 @@ function App() {
         )}
         {isMintButtonVisible && (
           <div className="my-auto mx-auto flex flex-col items-center">
-            <Countdown
-              date={new Date("April 20, 2023 16:20:00")}
-              renderer={(k) => (
-                <div className="text-6xl font-bold text-black flex flow-row my-5">
-                  <h1 className=" text-[#30B922] mx-4">{k.hours}</h1>:
-                  <h1 className=" text-[#ffbe0b] mx-4">{k.minutes}</h1>:
-                  <h1 className=" text-[#FF5903] mx-4">{k.seconds}</h1>
-                </div>
-              )}
-            />
             <h2 className="w-full text-black text-3xl font-bold animate-pulse text-center leading-10">
-              Go get your Lollypops 🍭
+              Seems like your wallet is missing a Lollypop. <br /> Get one now
+              🍭
             </h2>
             <button
-              onClick={handleOpenCollectionUrl}
+              onClick={handleOpenLollypop}
               className="rounded-xl bg-gradient-to-br from-[#6025F5] to-[#FF5555] px-10 py-3 text-2xl my-5 font-medium text-white transition duration-200 hover:shadow-lg hover:shadow-[#6025F5]/50"
             >
               #mintit
             </button>
           </div>
         )}
-        {/* {isUnrolledJointVisible && (
-          <div className="my-auto mx-auto flex flex-col items-center relative">
-            <Countdown
-              date={new Date("April 20, 2023 16:20:00")}
-              renderer={(k) => (
-                <div className="text-6xl font-bold text-white flex flow-row">
-                  <h1 className=" text-[#30B922] mx-4">{k.hours}</h1>:
-                  <h1 className=" text-[#FCE8AD] mx-4">{k.minutes}</h1>:
-                  <h1 className=" text-[#FF5903] mx-4">{k.seconds}</h1>
-                </div>
-              )}
-            />
-          </div>
-        )} */}
-
-        {/* {isUnrolledJointVisible ? (
+        {isUnrolledJointVisible ? (
           <div className="my-auto mx-auto flex flex-col items-center relative">
             <img src={jointUnrolled} alt="Roll it up" className="w-96 h-96 " />
             <div
@@ -182,9 +194,21 @@ function App() {
           isJointLit && (
             <div className="my-auto mx-auto flex flex-col items-center relative">
               <img src={jointLit} alt="Lit!" className="w-96 h-96 " />
+              <h2 className="w-full text-white text-3xl font-bold animate-pulse text-center">
+                Now that your Lolly is Lit 🔥, here's a Lolly Puff
+              </h2>
+              <button
+                onClick={handleOpenLollypuff}
+                className="rounded-xl bg-gradient-to-br from-[#6025F5] to-[#FF5555] px-10 py-3 text-2xl my-5 font-medium text-white transition duration-200 hover:shadow-lg hover:shadow-[#6025F5]/50"
+              >
+                #mintit
+              </button>
+              <p className="w-full text-white text-base font-bold text-center">
+                Note: Allowlist gets updated every few hours.
+              </p>
             </div>
           )
-        )} */}
+        )}
       </div>
     </ReactFlashlight>
   );
